a1ee557678961408bc476d311bff78db0c2f9fa4
sunyang <sun.yang@husky.neu.edu>
2019-10-06 22:09:07 -0700

    fix request number

--- java-client-generated/src/main/java/io/swagger/client/measurement/Metrics.java
+++ java-client-generated/src/main/java/io/swagger/client/measurement/Metrics.java
@@ -10,8 +10,6 @@ public class Metrics {
   private AtomicLong unsuccessfulRequest;
   private List<Long> latency;
   private List<String> record;
-//  private ConcurrentHashMap<Integer, Integer> graph;
-//  private Long globalStartTime;
 
   public List<String> getRecord() {
     return record;
@@ -30,21 +28,9 @@ public class Metrics {
     return latency;
   }
 
-//  public ConcurrentHashMap<Integer, Integer> getGraph() {
-//    return graph;
-//  }
-//
-//  public Long getGlobalStartTime() {
-//    return globalStartTime;
-//  }
-
-
-
 
   public Metrics(){
     this.latency = Collections.synchronizedList(new LinkedList<Long>());
-//    this.graph = new ConcurrentHashMap<>();
-//    globalStartTime = System.currentTimeMillis();
     this.record = Collections.synchronizedList(new LinkedList<String>());
     successfulRequest = new AtomicLong();
     unsuccessfulRequest = new AtomicLong();
